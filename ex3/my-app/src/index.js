475e49d4968072d7ac1552d0b15a19dcfae8b669
donovan <donovan.ferre@outlook.fr>
2018-01-31 15:31:02 +0100

    ex 2 fini

--- ex3/my-app/src/index.js
+++ ex3/my-app/src/index.js
@@ -11,7 +11,7 @@ class App extends React.Component{
     render(){
         return (
             <div>
-                <p>c'est parti !!</p>
+                <p> Vos recettes de cuisines</p>
                 <button></button>
                 </div>
         )
